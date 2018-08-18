9182d5625994e7043df1329e9d83dcd12fb90e70
Neeraj Kaushik <neerajkaushi@gmail.com>
2018-08-18 11:31:37 +0200

    Powershell script for reading data from Excel file

---
+++ Powershell Scripts/ReadDataFromExcel.ps1
@@ -0,0 +1,31 @@
+﻿$strPath = "<EXCEL FILE>"
+$outpath = "<OUTFILE>>"
+
+$objExcel = New-Object -ComObject Excel.Application
+$objExcel.Visible = $false
+
+$WorkBook = $objExcel.Workbooks.Open($strPath)
+$worksheet = $workbook.sheets.item("DEH List")
+$intRowMax =  ($worksheet.UsedRange.Rows).count
+$intColMax =  ($worksheet.UsedRange.Columns).count
+
+
+<#Write "`nServer Name     Server Function     1st Contact          2nd Contact" | Out-File $AssetInv#>
+Write "--------------- ------------------- -------------------- -----------------------------" | 
+      Out-FIle $AssetInv -Append
+
+Write-Host "Processing: " $intRowMax " rows" "and columns " $intColMax    
+
+for ($intRow = 1 ; $intRow -le $intRowMax ; $intRow++) {
+    
+     for($col =1 ; $col -le $intColMax ; $col++) 
+     {
+              
+            $cellValue = $worksheet.cells.item($intRow, $col).value2
+
+             "Cell ( " + $intRow + "," + $col + ") : =" + $cellValue |
+                Out-File $outpath -Append
+     }
+ }
+
+$objexcel.quit()
